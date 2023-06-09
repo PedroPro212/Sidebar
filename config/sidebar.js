f5eb13dccc2cf37c13ec2dc285303ae316198258
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2023-06-08 21:13:58 -0300

    style
    Escolher tema

---
+++ config/sidebar.js
@@ -0,0 +1,8 @@
+function escolherTema(){
+    var tema = document.getElementById('tema');
+
+    if(tema.style.display == 'block')
+        tema.style.display = 'none';
+    else
+        tema.style.display = 'block'
+}
